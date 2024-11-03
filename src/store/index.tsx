7f981cef60197ac881857d2746d7eaca8945a14e
CodePsy-2001 <codepsy2001@gmail.com>
2024-11-03 21:40:25 +0900

    최적화를 위해 미사용 store 제거 (remvoe zustand)

--- src/store/index.tsx
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-import * as T from '@/types';
-
-export const useTestState = create<T.TestState>(set => ({
-  name: 'test',
-  setName: name => set({ name }),
-}));
