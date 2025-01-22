6f96f4d769ad5bbc82e7d06e412aba4b08e57f48
DavinLee <davin11992@gmail.com>
2025-01-22 14:17:33 +0900

    Add blog header

--- components/Blog/BlogHeader.tsx
+++ components/Blog/BlogHeader.tsx
@@ -7,11 +7,12 @@ import SearchIcon from '../../assets/icon/blog_search.svg';
 
 const BlogHeader = () => {
   return (
-    <header className="flex items-center justify-between px-6 py-3.5 bg-white border-b border-gdsc-Grey-200">
+    <header className="flex items-center justify-between px-14 py-3.5 bg-white border-b border-gdsc-Grey-200">
       {/* 로고 및 제목 */}
       <div className="flex items-center gap-4">
-        <Link href="/blog/">
+        <Link href="/blog/" className="flex items-center gap-2">
           <Image src={HeaderLogo} alt="GDGoC Sogang logo" className="h-5.5"/>
+          <span className="text-gdsc-Grey-800 text-[21px] font-medium">공식 블로그</span>
         </Link>
       </div>
 
--- components/Blog/LoginButton.tsx
+++ components/Blog/LoginButton.tsx
@@ -7,7 +7,7 @@ interface RedButtonWithHrefLinkProps extends React.PropsWithChildren {
 
 const LoginButton = ({ children = "로그인", hrefLink, className }: ButtonWithHrefLinkProps) => {
   return (
-    <Link href={hrefLink} className={`bg-gdsc-Red-500 text-center rounded-lg px-[16px] py-[8px] mobile:px-5 mobile:py-3 text-gdsc-White text-[13px] mobile:text-16 w-fit border border-gdsc-White hover:border-gdsc-White hover:text-gdsc-White transition-colors ${className ?? 'hover:bg-gdsc-Red-700'}`}>
+    <Link href={hrefLink} className={`bg-gdsc-Red-500 text-center rounded-lg px-[16px] py-[8px] mobile:px-5 mobile:py-3 text-gdsc-White text-[14px] mobile:text-16 w-fit border border-gdsc-White hover:border-gdsc-White hover:text-gdsc-White transition-colors ${className ?? 'hover:bg-gdsc-Red-700'}`}>
       {children}
     </Link>
   );
