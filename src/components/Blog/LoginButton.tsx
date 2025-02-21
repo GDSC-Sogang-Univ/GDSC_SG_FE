3e1562f9d9457b2a37b2c68096343d48922016e5
naya-h2 <hw7666@naver.com>
2025-02-21 18:01:18 +0900

    feat: pressed header UI

--- src/components/Blog/LoginButton.tsx
+++ src/components/Blog/LoginButton.tsx
@@ -9,7 +9,7 @@ const LoginButton = ({ children = '로그인', hrefLink, className }: RedButtonW
   return (
     <Link
       href={hrefLink}
-      className={`bg-gdsc-Red-500 text-center rounded-lg px-[16px] py-[8px] mobile:px-5 mobile:py-3 text-gdsc-White text-[14px] mobile:text-16 w-fit border border-gdsc-White hover:border-gdsc-White hover:text-gdsc-White transition-colors ${className ?? 'hover:bg-gdsc-Red-700'}`}
+      className={`bg-gdsc-Red-500 text-center rounded-lg whitespace-nowrap px-[16px] py-[8px] mobile:px-5 mobile:py-3 text-gdsc-White text-[14px] mobile:text-16 w-fit border border-gdsc-White hover:border-gdsc-White hover:text-gdsc-White transition-colors ${className ?? 'hover:bg-gdsc-Red-700'}`}
     >
       {children}
     </Link>
