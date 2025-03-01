10b3dfb192e00885ea4af187e1c3e57857c0cb6e
naya-h2 <hw7666@naver.com>
2025-03-01 19:09:31 +0900

    design: 카드형 목록 레이아웃 UI

--- src/app/blog/(components)/BlogPostList.tsx
+++ src/app/blog/(components)/BlogPostList.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import CardIconBtn from '@/components/Blog/PostList/CardIconBtn';
+import ListIconBtn from '@/components/Blog/PostList/ListIconBtn';
 import VerticalCard from '@/components/Blog/VerticalCard';
+import { useState } from 'react';
 
 interface Props {
   data: any[];
@@ -8,24 +13,50 @@ interface Props {
  * post 레이아웃
  */
 const BlogPostList = ({ data }: Props) => {
+  const [viewType, setViewType] = useState<'card' | 'list'>('card');
+  const [sortType, setSortType] = useState<'latest' | 'popular'>('latest');
+
   return (
     <main className='flex flex-col gap-6 pt-9 px-[76px] w-full'>
-      <div>위에 버튼들</div>
-      <ul className='grid grid-cols-3 gap-10 w-full'>
-        {data.map(post => (
-          <li key={post.slug}>
-            <VerticalCard
-              category={post.category}
-              title={post.title}
-              date={post.date}
-              author={post.author}
-              hearts={6}
-              comments={14}
-              thumbnail={'/og-image.png'}
-            />
-          </li>
-        ))}
-      </ul>
+      {/* btn control bar */}
+      <section className='flex gap-6 items-center'>
+        {/* view */}
+        <div className='flex gap-6'>
+          <CardIconBtn onClick={() => setViewType('card')} isSelected={viewType === 'card'} />
+          <ListIconBtn onClick={() => setViewType('list')} isSelected={viewType === 'list'} />
+        </div>
+
+        <div className='bg-gdsc-Grey-200 w-[1px] h-5' />
+
+        {/* sort */}
+        <div className='flex gap-3 text-gdsc-Grey-400'>
+          <button onClick={() => setSortType('latest')} className={sortType === 'latest' ? 'text-gdsc-Grey-700' : ''}>
+            최신순
+          </button>
+          <button onClick={() => setSortType('popular')} className={sortType === 'popular' ? 'text-gdsc-Grey-700' : ''}>
+            인기순
+          </button>
+        </div>
+      </section>
+
+      {/* card list */}
+      {viewType === 'card' && (
+        <ul className='grid grid-cols-3 gap-10 w-full'>
+          {data.map(post => (
+            <li key={post.slug}>
+              <VerticalCard
+                category={post.category}
+                title={post.title}
+                date={post.date}
+                author={post.author}
+                hearts={6}
+                comments={14}
+                thumbnail={'/og-image.png'}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 };
