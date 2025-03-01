557f9f1796baf83ee434b902d265bf8630fffb0b
naya-h2 <hw7666@naver.com>
2025-03-01 21:44:27 +0900

    feat: mobile 검색창 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,20 @@
 import { getPostList } from '@/utils/blog/getPostList';
 import BlogPostList from './(components)/BlogPostList';
 import SideWrapper from './(components)/SideWrapper';
+import MobileSearchBar from '@/components/Blog/Header/MobileSearchBar';
+import { getPostTagList } from '@/utils/blog/getPostTagList';
 
 async function BlogMainPage() {
   const posts = await getPostList();
+  const tags = await getPostTagList();
 
   return (
     <div className='flex'>
       <SideWrapper />
-      <BlogPostList data={posts} />
+      <div className='w-full tablet_v:w-full !max-w-[1440px] pt-9 px-[76px] tablet_h:px-7 tablet_h:pt-6 mobile:px-5 mobile:pt-5'>
+        <MobileSearchBar tagList={tags} />
+        <BlogPostList data={posts} />
+      </div>
     </div>
   );
 }
