be86c2d1980f5e2af32b734c7cccf6549cc5224c
naya-h2 <hw7666@naver.com>
2025-03-02 03:19:00 +0900

    design: view, sort 쿼리 형식으로 변경

--- src/app/blog/(components)/BlogPostList.tsx
+++ src/app/blog/(components)/BlogPostList.tsx
@@ -3,9 +3,10 @@
 import CardIconBtn from '@/components/Blog/PostList/CardIconBtn';
 import ListIconBtn from '@/components/Blog/PostList/ListIconBtn';
 import VerticalCard from '@/components/Blog/PostCard/VerticalCard';
-import { useState } from 'react';
 import HorizonCard from '@/components/Blog/PostCard/HorizonCard';
 import { PostType } from '@/types/blog';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Fragment } from 'react';
 
 interface Props {
   data: PostType[];
@@ -15,8 +16,10 @@ interface Props {
  * post 레이아웃
  */
 const BlogPostList = ({ data }: Props) => {
-  const [viewType, setViewType] = useState<'card' | 'list'>('card');
-  const [sortType, setSortType] = useState<'latest' | 'popular'>('latest');
+  const params = useSearchParams();
+  const viewType = params.get('view') ?? 'card';
+  const sortType = params.get('sort') ?? 'latest';
+  const router = useRouter();
 
   return (
     <main className='flex flex-col gap-6 w-full'>
@@ -24,20 +27,29 @@ const BlogPostList = ({ data }: Props) => {
       <section className='flex gap-6 items-center tablet_h:justify-end'>
         {/* view */}
         <div className='flex gap-6 tablet_h:hidden'>
-          <CardIconBtn onClick={() => setViewType('card')} isSelected={viewType === 'card'} />
-          <ListIconBtn onClick={() => setViewType('list')} isSelected={viewType === 'list'} />
+          <CardIconBtn
+            onClick={() => router.push(`/blog?view=card&sort=${sortType}`)}
+            isSelected={viewType === 'card'}
+          />
+          <ListIconBtn
+            onClick={() => router.push(`/blog?view=list&sort=${sortType}`)}
+            isSelected={viewType === 'list'}
+          />
         </div>
 
         <div className='bg-gdsc-Grey-200 w-[1px] h-5 tablet_h:hidden' />
 
         {/* sort */}
         <div className='flex gap-3 text-gdsc-Grey-400 tablet_h:text-14'>
-          <button onClick={() => setSortType('latest')} className={sortType === 'latest' ? 'text-gdsc-Grey-700' : ''}>
+          <button
+            onClick={() => router.push(`/blog?view=${viewType}&sort=latest`)}
+            className={sortType === 'latest' ? 'text-gdsc-Grey-700' : ''}
+          >
             최신순
           </button>
           <button
             disabled
-            onClick={() => setSortType('popular')}
+            onClick={() => router.push(`/blog?view=${viewType}&sort=popular`)}
             className={`cursor-not-allowed ${sortType === 'popular' ? 'text-gdsc-Grey-700' : ''}`}
           >
             인기순
@@ -47,25 +59,44 @@ const BlogPostList = ({ data }: Props) => {
 
       {/* card list */}
       <ul
-        className={`w-full ${viewType === 'card' && 'grid grid-cols-3 gap-10'} ${viewType === 'list' && 'flex flex-col gap-12 mobile:gap-5'}`}
+        className={`w-full ${viewType === 'card' && 'grid grid-cols-3 gap-10'} ${viewType === 'list' && 'flex flex-col gap-12 mobile:gap-5'} tablet_h:flex tablet_h:flex-col tablet_h:gap-10`}
       >
         {data.map(post => (
-          <li key={post.slug}>
-            {viewType === 'card' && (
-              <VerticalCard
-                category={post.category}
-                title={post.title}
-                date={post.date}
-                author={post.author}
-                hearts={6}
-                comments={14}
-                thumbnail={
-                  'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-nkpZ6mzVDnmyfZkwRMRnrl2sUNxpX23a73sX6ZjEF82YVog7XZfu-k4TqGl3fQLMZRCe_AoWs5HPo__ktt4Ei_I8K-_wwftNhwNxbw'
-                }
-              />
-            )}
+          <Fragment key={post.slug}>
+            {/* pc ver. */}
+            <li className='tablet_h:hidden'>
+              {viewType === 'card' && (
+                <VerticalCard
+                  category={post.category}
+                  title={post.title}
+                  date={post.date}
+                  author={post.author}
+                  hearts={6}
+                  comments={14}
+                  thumbnail={
+                    'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-nkpZ6mzVDnmyfZkwRMRnrl2sUNxpX23a73sX6ZjEF82YVog7XZfu-k4TqGl3fQLMZRCe_AoWs5HPo__ktt4Ei_I8K-_wwftNhwNxbw'
+                  }
+                />
+              )}
 
-            {viewType === 'list' && (
+              {viewType === 'list' && (
+                <HorizonCard
+                  category={post.category}
+                  title={post.title}
+                  date={post.date}
+                  author={post.author}
+                  hearts={6}
+                  comments={14}
+                  thumbnail={
+                    'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-nkpZ6mzVDnmyfZkwRMRnrl2sUNxpX23a73sX6ZjEF82YVog7XZfu-k4TqGl3fQLMZRCe_AoWs5HPo__ktt4Ei_I8K-_wwftNhwNxbw'
+                  }
+                  description={post.description}
+                />
+              )}
+            </li>
+
+            {/* mobile ver. */}
+            <li className='hidden tablet_h:block'>
               <HorizonCard
                 category={post.category}
                 title={post.title}
@@ -78,8 +109,8 @@ const BlogPostList = ({ data }: Props) => {
                 }
                 description={post.description}
               />
-            )}
-          </li>
+            </li>
+          </Fragment>
         ))}
       </ul>
     </main>
