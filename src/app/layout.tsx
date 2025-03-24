1de22df8b19528f445a41986fec5a5cb3ea39e80
naya-h2 <hw7666@naver.com>
2025-03-25 02:25:18 +0900

    feat: 파비콘 화질 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,9 @@ export const metadata: Metadata = {
   title: 'GDG on Campus Sogang',
   description: 'Google Developer Groups on Sogang University',
   icons: {
-    icon: 'favicon.ico',
+    icon: '/gdg_logo.svg',
+    shortcut: '/gdg_sg_logo.svg',
+    apple: '/gdg_sg_logo.svg',
   },
   openGraph: {
     url: 'https://gdg-campus-sg.com/',
@@ -15,8 +17,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: 'https://gdg-campus-sg.com/og-image.png',
-        width: 1200,
-        height: 630,
         alt: 'GDG on Campus : Sogang Univ',
       },
     ],
