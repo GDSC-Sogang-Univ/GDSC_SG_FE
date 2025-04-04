616c2bc01cbf3f35bd8874b6fab74fda6ddef5e1
naya-h2 <hw7666@naver.com>
2025-04-04 18:51:49 +0900

    design: 검색 결과 화면 UI

--- src/app/blog/search/(components)/SearchLayout.tsx
+++ src/app/blog/search/(components)/SearchLayout.tsx
@@ -3,19 +3,25 @@
 import { PostType } from '@/types/blog';
 import { useSearchParams } from 'next/navigation';
 import BlogPostList from '../../(components)/BlogPostList';
+import MobileSearchBar from '@/components/Blog/Header/MobileSearchBar';
 
 interface Props {
   postData: PostType[];
+  tags: string[];
 }
 
-const SearchLayout = ({ postData }: Props) => {
+const SearchLayout = ({ postData, tags }: Props) => {
   const params = useSearchParams();
   const filteredPost = postData.filter(post => post.title.toLowerCase().includes(params.get('q')?.toLowerCase() ?? ''));
 
   return (
     <>
-      <h1 className='text-center text-48 font-bold'>🔎 {params.get('q')}</h1>
-      {filteredPost.length > 0 ? <BlogPostList data={filteredPost} /> : '검색 결과가 없습니다.'}
+      <h1 className='text-center text-48 font-bold bg-[#F1F3F4] py-[76px] cp_mobile:text-32'><span className='text-gdsc-Red-500'>{params.get('q')}</span> 에 대한 결과</h1>
+      
+      <div className='w-full tablet_v:w-full !max-w-[1440px] py-14 px-[76px] tablet_h:px-7 tablet_h:py-6 cp_mobile:px-5 cp_mobile:py-5'>
+        <MobileSearchBar tagList={tags} />
+        {filteredPost.length > 0 ? <BlogPostList data={filteredPost} /> : '검색 결과가 없습니다.'}
+      </div>
     </>
   );
 };
--- src/app/blog/tag/[tagName]/page.tsx
+++ src/app/blog/tag/[tagName]/page.tsx
@@ -1,6 +1,7 @@
 import { getPostList } from '@/utils/blog/getPostList';
 import { getPostTagList } from '@/utils/blog/getPostTagList';
 import BlogPostList from '../../(components)/BlogPostList';
+import MobileSearchBar from '@/components/Blog/Header/MobileSearchBar';
 
 export async function generateMetadata(props: any) {
   const params = await props.params;
@@ -21,11 +22,16 @@ const TagPage = async ({ params }: { params: any }) => {
   const tagName = decodeURIComponent(params.tagName);
   const posts = await getPostList();
   const filteredPost = posts.filter(post => post.tags.includes(tagName));
+  const tags = await getPostTagList();
 
   return (
     <>
-      <h1 className='text-center text-48 font-bold'># {tagName}</h1>
-      <BlogPostList data={filteredPost} />
+      <h1 className='text-center text-48 font-bold bg-[#F1F3F4] py-[76px] cp_mobile:text-24'><span className='text-gdsc-Red-500'>#{tagName}</span> 에 대한 결과</h1>
+
+      <div className='w-full tablet_v:w-full !max-w-[1440px] py-14 px-[76px] tablet_h:px-7 tablet_h:py-6 cp_mobile:px-5 cp_mobile:py-5'>
+        <MobileSearchBar tagList={tags} />
+        {filteredPost.length > 0 ? <BlogPostList data={filteredPost} /> : '검색 결과가 없습니다.'}
+      </div>
     </>
   );
 };
