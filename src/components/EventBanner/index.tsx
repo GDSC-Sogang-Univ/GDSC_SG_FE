83317fefae79400c28056ef4c7074d4da206593d
naya-h2 <hw7666@naver.com>
2025-06-09 17:22:38 +0900

    feat: 이벤트 배너 데이터 등록

--- src/components/EventBanner/index.tsx
+++ src/components/EventBanner/index.tsx
@@ -1,10 +1,24 @@
 import Banner from '../ui/banner/Banner';
-import backendSeminar from '@/assets/images/backendSeminar.png';
 
 const bannerContent: BannerContent[] = [
-  { title: '라인플러스 백엔드 엔지니어 현직자 커리어 세미나(24.11.27)', imageSrc: backendSeminar },
-  { title: '라인플러스 백엔드 엔지니어 현직자 커리어 세미나(24.11.28)', imageSrc: backendSeminar },
-  { title: '라인플러스 백엔드 엔지니어 현직자 커리어 세미나(24.11.29)', imageSrc: backendSeminar },
+  {
+    title: '라인플러스 백엔드 엔지니어 현직자 커리어 세미나',
+    date: '24.11.27',
+    imageSrc: '/banner/career2.png',
+    link: 'https://www.instagram.com/p/DCDpx_DJbAX/?igsh=NHl2ZDBscnFkd2ty',
+  },
+  {
+    title: '제 1회 허들톤 GDGoC X CNU',
+    date: '24.10.28 ~ 11.29',
+    imageSrc: '/banner/hurdlethon.png',
+    link: 'https://www.instagram.com/p/DDQi9LtBWXB/?igsh=ajJranpnYmh4c3o=',
+  },
+  {
+    title: '당근 프론트엔드 엔지니어 현직자 커리어 세미나',
+    date: '24.10.28',
+    imageSrc: '/banner/career1.png',
+    link: 'https://www.instagram.com/p/DA5YuRhpyJy/?igsh=MW5rcm42aDZlZDJoaA==',
+  },
 ];
 
 const EventBanner = () => {
@@ -13,7 +27,15 @@ const EventBanner = () => {
       <p className='font-light text-gdsc-Grey-700 tablet_h:text-14'>가까운 행사</p>
       <section className='flex flex-col gap-9'>
         {bannerContent.map(content => {
-          return <Banner key={content.title} title={content.title} imageSrc={content.imageSrc} />;
+          return (
+            <Banner
+              key={content.title}
+              link={content.link}
+              date={content.date}
+              title={content.title}
+              imageSrc={content.imageSrc}
+            />
+          );
         })}
       </section>
     </div>
