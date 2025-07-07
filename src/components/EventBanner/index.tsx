6cb9626852aadcba8086d83ddfa44b09b60febd0
naya-h2 <hw7666@naver.com>
2025-07-07 21:44:01 +0900

    feat: 이벤트배너 데이터 업데이트

--- src/components/EventBanner/index.tsx
+++ src/components/EventBanner/index.tsx
@@ -2,29 +2,29 @@ import Banner from '../ui/banner/Banner';
 
 const bannerContent: BannerContent[] = [
   {
-    title: '라인플러스 백엔드 엔지니어 현직자 커리어 세미나',
-    date: '24.11.27',
-    imageSrc: '/banner/career2.png',
-    link: 'https://www.instagram.com/p/DCDpx_DJbAX/?igsh=NHl2ZDBscnFkd2ty',
+    title: 'Build With AI For Everyone',
+    date: '25.05.17',
+    imageSrc: '/banner/bai.png',
+    link: 'https://www.instagram.com/p/DKLr3UnhCH9/?img_index=1',
   },
   {
-    title: '제 1회 허들톤 GDGoC X CNU',
-    date: '24.10.28 ~ 11.29',
-    imageSrc: '/banner/hurdlethon.png',
-    link: 'https://www.instagram.com/p/DDQi9LtBWXB/?igsh=ajJranpnYmh4c3o=',
+    title: 'Membership Training',
+    date: '25.03.28',
+    imageSrc: '/banner/mt.png',
+    link: 'https://www.instagram.com/p/DH8visJSZnU/?img_index=1',
   },
   {
-    title: '당근 프론트엔드 엔지니어 현직자 커리어 세미나',
-    date: '24.10.28',
-    imageSrc: '/banner/career1.png',
-    link: 'https://www.instagram.com/p/DA5YuRhpyJy/?igsh=MW5rcm42aDZlZDJoaA==',
+    title: '개강총회',
+    date: '25.03.14',
+    imageSrc: '/banner/ot.png',
+    link: 'https://www.instagram.com/p/DHhhHs6hqom/?img_index=1',
   },
 ];
 
 const EventBanner = () => {
   return (
     <div className='p-9 flex flex-col gap-6 min-h-dvh tablet_h:p-6 tablet_h:gap-5'>
-      <p className='font-light text-gdsc-Grey-700 tablet_h:text-14'>가까운 행사</p>
+      <p className='font-light text-gdsc-Grey-700 tablet_h:text-14'>최근 행사</p>
       <section className='flex flex-col gap-9'>
         {bannerContent.map(content => {
           return (
